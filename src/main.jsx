cf6cc671b83f7f40bae5aba970b7035a2e834c05
김재환 <jaehwan01101@users.noreply.github.com>
2026-09-01 13:04:17 +0900

    초기 웹폰트 전환 현상 개선

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,23 @@ import "@fontsource-variable/jetbrains-mono";
 import { App } from "./App.jsx";
 import "./styles.css";
 
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+async function renderApp() {
+  if (document.fonts) {
+    const fontsReady = Promise.all([
+      document.fonts.load('400 1em "Hanken Grotesk Variable"', "Portfolio"),
+      document.fonts.load('400 1em "JetBrains Mono Variable"', "portfolio.log"),
+      document.fonts.load('400 1em "Pretendard Variable"', "김재환"),
+    ]);
+    const timeout = new Promise((resolve) => window.setTimeout(resolve, 3000));
+
+    await Promise.race([fontsReady, timeout]);
+  }
+
+  createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+}
+
+renderApp();
